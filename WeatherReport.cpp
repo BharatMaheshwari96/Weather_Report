66387477a094d2817611860bc18978f1af05b9e7
Bharat Maheshwari <59102335+BharatMaheshwari96@users.noreply.github.com>
2020-01-25 13:14:32 +0000

    Update WeatherReport.cpp

--- WeatherReport.cpp
+++ WeatherReport.cpp
@@ -1,6 +1,32 @@
 #include<iostream>
 using namespace std;
 
+class weather
+{
+private:	
+	int day_of_month[365];
+	float high_temp[365], low_temp[365], amt_rain[365], amt_snow[365];
+	float avg,ht,lt,r,s;
+	int mnth,d,i;
+	char ch;
+public:
+	weather()
+	{
+		for(i=0;i<=100;i++)
+		{
+		day_of_month[i]=99;
+		high_temp[i]=999; 
+		low_temp[i]=-999;
+		amt_rain[i]=0;
+		amt_snow[i]=0;
+		}
+	}
+	void display(int);
+	void accept();
+	void get_values(int);	
+
+};
+
 int main()
 {
 	int ch,month,flag=0;
